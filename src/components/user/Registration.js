68fb8d90c0152bc0e17c4734ddbfbfde1fc27fa0
ymcla <ymclapp@yahoo.com>
2022-01-27 11:06:02 -0600

    added submit function

--- src/components/user/Registration.js
+++ src/components/user/Registration.js
@@ -1,4 +1,5 @@
 import { useRef, useState, useEffect } from 'react';
+import axios from '../../api/axios';
 
 import './Registration.css';
 
@@ -9,6 +10,8 @@ import { Form, Button } from 'react-bootstrap';
 
 const USERNAME_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+const REGISTRATION_URL = '/auth/local/register';
 
 
 const Registration = () => {
@@ -55,103 +58,152 @@ const Registration = () => {
         setErrMsg('');
     }, [username, password, matchPassword])
 
-    return (
-        <section>
-            <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live='assertive'>{errMsg}</p>
-            <h1> Register</h1>
-            <Form>
+    const handleRegistrationSubmit = async (e) => {
+        e.preventDefault();
+        // if button enabled with JS hack - this will check one more time and won't say which is wrong
+        const v1 = USERNAME_REGEX.test(username);
+        const v2 = PASSWORD_REGEX.test(password);
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry");
+            return;
+        }
+        // console.log(username, password);
+        // setSuccess(true);
 
-                <Form.Label htmlFor='username'>
-                    Username:
-                    <span className={validName ? 'valid' : 'hide'}>
-                        <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    <span className={validName || !username ? 'hide' : 'invalid'}>
-                        <FontAwesomeIcon icon={faTimes} />
-                    </span>
-                </Form.Label>
-                <Form.Control
-                    type='text'
-                    id='username'
-                    ref={usernameRef}
-                    autoComplete='off'
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                    aria-invalid={validName ? 'false' : 'true'}
-                    aria-describedby='uidnote'
-                    onFocus={() => setUsernameFocus(true)}
-                    onBlur={() => setUsernameFocus(false)}
-                />
-                <p id='uidnote' className={usernameFocus && username &&
-                    !validName ? 'instructions' : 'offscreen'}>
-                    <FontAwesomeIcon icon={faInfoCircle} />
-                    4 to 24 characters.<br />
-                    Must begin with a letter.<br />
-                    Letters, numbers, underscores, hyphens allowed.
-                </p>
-
-                <Form.Label htmlFor='password'>
-                    Password:
-                    <span className={validPassword ? 'valid' : 'hide'}>
-                        <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    <span className={validPassword || !password ? 'hide' : 'invalid'}>
-                        <FontAwesomeIcon icon={faTimes} />
-                    </span>
-                </Form.Label>
-                <Form.Control
-                    type='password'
-                    id='password'
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    aria-invalid={validPassword ? 'false' : 'true'}
-                    aria-describedby='passwordnote'
-                    onFocus={() => setPasswordFocus(true)}
-                    onBlur={() => setPasswordFocus(false)}
-                />
-                <p id='passwordnote' className={passwordFocus && !validPassword ? 'instructions' : 'offscreen'}>
-                    <FontAwesomeIcon icon={faInfoCircle} />
-                    8 to 24 characters.<br />
-                    Must include uppercase and lowercase letters, a number and a special character.<br />
-                    Allowed special characters:  <span aria-label='exclamation mark'>!</span> <span aria-label='at symbol'>@</span> <span aria-label='hashtag'>#</span> <span aria-label='dollar sign'>$</span> <span aria-label='percent'>%</span>
-                </p>
-
-                <Form.Label htmlFor='confirm_password'>
-                    Confirm Password:
-                    <span className={validMatch && matchPassword ? 'valid' : 'hide'}>
-                        <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    <span className={validMatch || !matchPassword ? 'hide' : 'invalid'}>
-                        <FontAwesomeIcon icon={faTimes} />
-                    </span>
-                </Form.Label>
-                <Form.Control
-                    type='password'
-                    id='confirm_password'
-                    onChange={(e) => setMatchPassword(e.target.value)}
-                    required
-                    aria-invalid={validMatch ? 'false' : 'true'}
-                    aria-describedby='confirmnote'
-                    onFocus={() => setMatchFocus(true)}
-                    onBlur={() => setMatchFocus(false)}
-                />
-                <p id='confirmnote' className={matchFocus && !validMatch ? 'instructions' : 'offscreen'}>
-                    <FontAwesomeIcon icon={faInfoCircle} />
-                    Must match the first password input field.
-                </p>
-
-                <Button disabled={!validName || !validPassword || !validMatch ? true : false}>Create Account</Button>
-            </Form>
-
-            <p>
-                Already register?<br />
-                <span className='line'>
-                    <a href='/login'>Sign In</a>
-                </span>
-            </p>
-        </section>
-    )
+        try {
+            const response = await axios.post(REGISTRATION_URL,
+                JSON.stringify({ username, password }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    // withCredentials:  true
+                }
+            );
+            console.log(response.data);
+            console.log(response.data.jwt);
+            console.log(JSON.stringify(response))
+            setSuccess(true);
+            //clear input fields
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 409) {
+                setErrMsg('Username already taken');
+            } else {
+                setErrMsg('Registration Failed');
+            }
+            errRef.current.focus();
+        }
+    }
 }
+
+return (
+    <>
+        {success ? (
+            <section>
+                <h1>Success!</h1>
+                <p>
+                    <a href='/login'>Sign In</a>
+                </p>
+            </section>
+        ) : (
+            <section>
+                <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live='assertive'>{errMsg}</p>
+                <h1> Register</h1>
+                <Form onSubmit={handleRegistrationSubmit}>
+
+                    <Form.Label htmlFor='username'>
+                        Username:
+                        <span className={validName ? 'valid' : 'hide'}>
+                            <FontAwesomeIcon icon={faCheck} />
+                        </span>
+                        <span className={validName || !username ? 'hide' : 'invalid'}>
+                            <FontAwesomeIcon icon={faTimes} />
+                        </span>
+                    </Form.Label>
+                    <Form.Control
+                        type='text'
+                        id='username'
+                        ref={usernameRef}
+                        autoComplete='off'
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                        aria-invalid={validName ? 'false' : 'true'}
+                        aria-describedby='uidnote'
+                        onFocus={() => setUsernameFocus(true)}
+                        onBlur={() => setUsernameFocus(false)}
+                    />
+                    <p id='uidnote' className={usernameFocus && username &&
+                        !validName ? 'instructions' : 'offscreen'}>
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                        4 to 24 characters.<br />
+                        Must begin with a letter.<br />
+                        Letters, numbers, underscores, hyphens allowed.
+                    </p>
+
+                    <Form.Label htmlFor='password'>
+                        Password:
+                        <span className={validPassword ? 'valid' : 'hide'}>
+                            <FontAwesomeIcon icon={faCheck} />
+                        </span>
+                        <span className={validPassword || !password ? 'hide' : 'invalid'}>
+                            <FontAwesomeIcon icon={faTimes} />
+                        </span>
+                    </Form.Label>
+                    <Form.Control
+                        type='password'
+                        id='password'
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        aria-invalid={validPassword ? 'false' : 'true'}
+                        aria-describedby='passwordnote'
+                        onFocus={() => setPasswordFocus(true)}
+                        onBlur={() => setPasswordFocus(false)}
+                    />
+                    <p id='passwordnote' className={passwordFocus && !validPassword ? 'instructions' : 'offscreen'}>
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                        8 to 24 characters.<br />
+                        Must include uppercase and lowercase letters, a number and a special character.<br />
+                        Allowed special characters:  <span aria-label='exclamation mark'>!</span> <span aria-label='at symbol'>@</span> <span aria-label='hashtag'>#</span> <span aria-label='dollar sign'>$</span> <span aria-label='percent'>%</span>
+                    </p>
+
+                    <Form.Label htmlFor='confirm_password'>
+                        Confirm Password:
+                        <span className={validMatch && matchPassword ? 'valid' : 'hide'}>
+                            <FontAwesomeIcon icon={faCheck} />
+                        </span>
+                        <span className={validMatch || !matchPassword ? 'hide' : 'invalid'}>
+                            <FontAwesomeIcon icon={faTimes} />
+                        </span>
+                    </Form.Label>
+                    <Form.Control
+                        type='password'
+                        id='confirm_password'
+                        onChange={(e) => setMatchPassword(e.target.value)}
+                        required
+                        aria-invalid={validMatch ? 'false' : 'true'}
+                        aria-describedby='confirmnote'
+                        onFocus={() => setMatchFocus(true)}
+                        onBlur={() => setMatchFocus(false)}
+                    />
+                    <p id='confirmnote' className={matchFocus && !validMatch ? 'instructions' : 'offscreen'}>
+                        <FontAwesomeIcon icon={faInfoCircle} />
+                        Must match the first password input field.
+                    </p>
+
+                    <Button type='submit' disabled={!validName || !validPassword || !validMatch ? true : false}>Create Account</Button>
+                </Form>
+
+                <p>
+                    Already register?<br />
+                    <span className='line'>
+                        <a href='/login'>Sign In</a>
+                    </span>
+                </p>
+            </section>
+        )}
+    </>
+)
+
 
 export default Registration;
 
