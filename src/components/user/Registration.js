abddd8d2be2594c3a9b4705240dbe047b066cf9a
ymcla <ymclapp@yahoo.com>
2022-01-27 10:26:44 -0600

    have all three fields in form with validation and instructions showing.  Added css

--- src/components/user/Registration.js
+++ src/components/user/Registration.js
@@ -1,3 +1,150 @@
 import { useRef, useState, useEffect } from 'react';
 
-import 
+import './Registration.css';
+
+import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { Form } from 'react-bootstrap';
+
+const USERNAME_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+
+const Registration = () => {
+
+    const usernameRef = useRef();
+    const errRef = useRef();
+
+    const [username, setUsername] = useState('');
+    const [validName, setValidName] = useState(false);
+    const [usernameFocus, setUsernameFocus] = useState(false);
+
+    const [password, setPassword] = useState('');
+    const [validPassword, setValidPassword] = useState(false);
+    const [passwordFocus, setPasswordFocus] = useState(false);
+
+    const [matchPassword, setMatchPassword] = useState('');
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        usernameRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        const result = USERNAME_REGEX.test(username);
+        console.log(result);
+        console.log(username);
+        setValidName(result);
+    }, [username])
+
+    useEffect(() => {
+        const result = PASSWORD_REGEX.test(password);
+        console.log(result);
+        console.log(password);
+        setValidPassword(result);
+        const match = password === matchPassword;
+        setValidMatch(match);
+    }, [password, matchPassword])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [username, password, matchPassword])
+
+    return (
+        <section>
+            <p ref={errRef} className={errMsg ? 'errmsg' : 'offscreen'} aria-live='assertive'>{errMsg}</p>
+            <h1> Register</h1>
+            <Form>
+
+                <Form.Label htmlFor='username'>
+                    Username:
+                    <span className={validName ? 'valid' : 'hide'}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span className={validName || !username ? 'hide' : 'invalid'}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                </Form.Label>
+                <Form.Control
+                    type='text'
+                    id='username'
+                    ref={usernameRef}
+                    autoComplete='off'
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                    aria-invalid={validName ? 'false' : 'true'}
+                    aria-describedby='uidnote'
+                    onFocus={() => setUsernameFocus(true)}
+                    onBlur={() => setUsernameFocus(false)}
+                />
+                <p id='uidnote' className={usernameFocus && username &&
+                    !validName ? 'instructions' : 'offscreen'}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    4 to 24 characters.<br />
+                    Must begin with a letter.<br />
+                    Letters, numbers, underscores, hyphens allowed.
+                </p>
+
+                <Form.Label htmlFor='password'>
+                    Password:
+                    <span className={validPassword ? 'valid' : 'hide'}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span className={validPassword || !password ? 'hide' : 'invalid'}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                </Form.Label>
+                <Form.Control
+                    type='password'
+                    id='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    aria-invalid={validPassword ? 'false' : 'true'}
+                    aria-describedby='passwordnote'
+                    onFocus={() => setPasswordFocus(true)}
+                    onBlur={() => setPasswordFocus(false)}
+                />
+                <p id='passwordnote' className={passwordFocus && !validPassword ? 'instructions' : 'offscreen'}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    8 to 24 characters.<br />
+                    Must include uppercase and lowercase letters, a number and a special character.<br />
+                    Allowed special characters:  <span aria-label='exclamation mark'>!</span> <span aria-label='at symbol'>@</span> <span aria-label='hashtag'>#</span> <span aria-label='dollar sign'>$</span> <span aria-label='percent'>%</span>
+                </p>
+
+                <Form.Label htmlFor='confirm_password'>
+                    Confirm Password:
+                    <span className={validMatch && matchPassword ? 'valid' : 'hide'}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span className={validMatch || !matchPassword ? 'hide' : 'invalid'}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                </Form.Label>
+                <Form.Control
+                    type='password'
+                    id='confirm_password'
+                    onChange={(e) => setMatchPassword(e.target.value)}
+                    required
+                    aria-invalid={validMatch ? 'false' : 'true'}
+                    aria-describedby='confirmnote'
+                    onFocus={() => setMatchFocus(true)}
+                    onBlur={() => setMatchFocus(false)}
+                />
+                <p id='confirmnote' className={matchFocus && !validMatch ? 'instructions' : 'offscreen'}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    Must match the first password input field.
+                </p>
+
+            </Form>
+        </section>
+    )
+}
+
+export default Registration;
+
+
